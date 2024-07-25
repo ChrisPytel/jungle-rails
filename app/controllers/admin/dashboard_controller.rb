7eb0ac42ca21f8ed67067cad0c8a17e60218ebca
ChrisPytel <chris_pytel@hotmail.com>
2024-07-24 23:33:41 -0400

    update admin dashboard views to display total count for categories and products

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   before_action :authenticate
   
   def show
+    @total_category_count = Category.count  
+    @total_product_count = Product.count
   end
+
 end
